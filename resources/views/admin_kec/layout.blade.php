2a36a657418318e8c7add235c14cd205ce4fc891
nurulaulia19 <nurulauliaseptiani9@gmail.com>
2024-08-07 09:30:27 +0700

    memperbaiki

--- resources/views/admin_kec/layout.blade.php
+++ resources/views/admin_kec/layout.blade.php
@@ -127,7 +127,7 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="info">
-                        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                        <p> <a href="#" class="d-block" style="font-size: 15px">{{ Auth::user()->name }}</a> </p>
                     </div>
                 </div>
 
